b72969094aee9a97cf14b4be6adfb5c166e1bb31
Shayegan8 <shayeganrood85@gmail.com>
2026-02-15 12:49:29 +0330

    server.ts with no env

--- src/server.ts
+++ src/server.ts
@@ -13,8 +13,8 @@ app.set("view engine", "ejs")
 app.use(express.urlencoded({ extended: true }))
 app.use(router)
 
-const server = app.listen(Config.port, () => {
-    console.log(`Server is running on port ${Config.port}`)
+const server = app.listen(Config.port || 3000, () => {
+    console.log(`Server is running on port ${Config.port || 3000}`)
 })
 
 export default server
